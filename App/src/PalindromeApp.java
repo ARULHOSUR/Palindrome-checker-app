2d819e2d9db060dc1696bae8e7229afa5ebbd31f
ARULHOSUR <arulhosur1609@gmail.com>
2026-03-03 14:54:14 +0530

    UC2 completed

--- App/src/PalindromeApp.java
+++ App/src/PalindromeApp.java
@@ -2,7 +2,23 @@ public class PalindromeApp {
     static void main() {
         System.out.println("WELCOME TO PALINDROME CHECKER MANAGEMENT SYSTEM");
                 System.out.println("VERSION 1.0");
-        System.out.println("System initiilized sucessfully")
+        System.out.println("System initiilized sucessfully");
+     String input ="MADAM";
+        boolean ispalindrome =true;
+        for(int i=0;i<input.length()/2;i++)
+        {
+            if(input.charAt(i) !=input.charAt(input.length()-1-i))
+            {
+                ispalindrome =false;
+                break;
 
+            }
+        }
+        if(ispalindrome) {
+            System.out.println(input + "is palindrome.");
+        }else{
+            System.out.println(input +"not a palindrome");
+        }
     }
 }
+
