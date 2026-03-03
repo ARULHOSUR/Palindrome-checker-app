045c4ee1e1350cd084b7d3bb783b4cdcdd5c6cfb
ARULHOSUR <arulhosur1609@gmail.com>
2026-03-03 14:29:27 +0530

    UC1 completed

---
+++ App/src/PalindromeApp.java
@@ -0,0 +1,8 @@
+public class PalindromeApp {
+    static void main() {
+        System.out.println("WELCOME TO PALINDROME CHECKER MANAGEMENT SYSTEM");
+                System.out.println("VERSION 1.0");
+        System.out.println("System initiilized sucessfully")
+
+    }
+}
